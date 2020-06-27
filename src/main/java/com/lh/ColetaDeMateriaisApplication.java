d1f8d24be146f44e40ce2e0896426a63faaa85e7
Luan Hitalo <hitaloluan@gmail.com>
2020-06-26 22:51:56 -0300

    att

---
+++ src/main/java/com/lh/ColetaDeMateriaisApplication.java
@@ -0,0 +1,13 @@
+package com.lh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class ColetaDeMateriaisApplication {
+     public static void main(String[] args) {
+        SpringApplication.run(ColetaDeMateriaisApplication.class, args);
+    }
+}
